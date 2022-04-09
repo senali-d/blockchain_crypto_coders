e9b45d8177c04e757defe45ccac72308a57a0a76
Senali Dilumika <senali.d@fidenz.com>
2022-04-09 23:36:07 +0530

    update test

--- contracts/CryptoCoders.sol
+++ contracts/CryptoCoders.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.0;
 import "../client/node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract CryptoCoders is ERC721 {
+
+  string[] public coders;
+
   constructor() ERC721("CryptoCoders", "CCS") {
   }
+
+  function mint(string memory coder) public {
+    coders.push(coder);
+  }
+
 }
--- test/cryptocoders.js
+++ test/cryptocoders.js
@@ -1,8 +1,21 @@
 const CryptoCoders = artifacts.require("./CryptoCoders.sol");
 
 contract("CryptoCoders", accounts => {
+
+  let contract;
+  before(async() => {
+    contract = await CryptoCoders.deployed()
+  })
+
   it("...get deploy.", async () => {
-    const contract = await CryptoCoders.deployed();
-    console.log("🚀 ~ contract", contract)
+    assert.notEqual(contract, "")
   });
+
+
+  it("...get's minted and added", async () => {
+    const result = await contract.mint("Alex");
+    let corder = await contract.coders(0)
+    console.log(corder)
+  })
+
 });
